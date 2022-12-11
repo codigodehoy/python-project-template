04ec9decf304f1be2902695d056b0f9bc2f8f21c
kaparoo <kaparoo2001@gmail.com>
2022-12-11 17:02:35 +0900

    ✅ Update tests to depend on pytest

--- tests/subtests/test_bar.py
+++ tests/subtests/test_bar.py
@@ -1,13 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from project_name.subpackage import bar
-import unittest
 
 
-class BarTest(unittest.TestCase):
-    def test_bar(self):
-        self.assertEqual(bar.return_bar(), "bar!")
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_bar():
+    assert bar.return_bar() == "bar!"
--- tests/test_foo.py
+++ tests/test_foo.py
@@ -1,16 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import unittest
-
 from project_name import foo
 
 
-class FooTest(unittest.TestCase):
-    def test_repeat_bar(self):
-        self.assertEqual(foo.repeat_bar(10), "bar!" * 10)
-        self.assertEqual(foo.repeat_bar(0), "")
-        self.assertEqual(foo.repeat_bar(-10), "")
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_repeat_bar():
+    assert foo.repeat_bar(10) == "bar!" * 10
+    assert foo.repeat_bar(0) == ""
+    assert foo.repeat_bar(-10) == ""
